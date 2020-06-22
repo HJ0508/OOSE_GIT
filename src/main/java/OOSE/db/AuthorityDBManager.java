dfbcafa72442a185a32dd6aeac91689d46ef4e27
jaehe <jaehe0413@naver.com>
2020-06-22 18:59:24 +0900

    권한 조회 추가

---
+++ src/main/java/OOSE/db/AuthorityDBManager.java
@@ -0,0 +1,35 @@
+package OOSE.db;
+
+import OOSE.model.Authority;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class AuthorityDBManager extends DBConnector
+{
+    public ArrayList<Authority> browseAuthority()
+    {
+        try
+        {
+            String query="select * from oose.authority";
+            pstmt = conn.prepareStatement(query);
+
+            ArrayList<Authority> list = new ArrayList<Authority>();
+            res = pstmt.executeQuery();
+            while(res.next())
+            {
+                Authority i = new Authority();
+                i.setId(res.getInt("authorityId"));
+                i.setName(res.getString("authorityName"));
+                i.setAccessRange(res.getString("accessRange"));
+                list.add(i);
+            }
+            return list;
+        }
+        catch(SQLException e)       //오류가 발생하면 null return
+        {
+            e.getStackTrace();
+            return null;
+        }
+    }
+}
